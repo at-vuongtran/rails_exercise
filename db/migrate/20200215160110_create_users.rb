e6876a5e2ad636f53df1efc37632b64adb7e71de
at-vuongtran <vuong.tran@asiantech.vn>
2020-02-15 23:06:01 +0700

    drop and recreate db

--- db/migrate/20200215160110_create_users.rb
+++ db/migrate/20200215160110_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :email
       t.string :name
       t.string :password, null: false
-      t.integer :team_id
 
       t.timestamps
     end
---
+++ db/migrate/20200215160129_create_roles.rb
@@ -0,0 +1,6 @@
+class CreateRoles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :roles do |t|
+    end
+  end
+end
