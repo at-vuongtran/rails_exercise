2f29d4f8a20b2e25eb92f81aa22fda28d1bb276b
at-vuongtran <vuong.tran@asiantech.vn>
2020-02-17 10:50:23 +0700

    def show and show html

--- app/controllers/users/sub_leaders_controller.rb
+++ app/controllers/users/sub_leaders_controller.rb
@@ -4,6 +4,10 @@ class Users::SubLeadersController < ApplicationController
   end
 
   def show
-    @sub_leader = (Role.includes(:user, :team).where(role: 2)).select { |item| item.user.id == params[:id] }
+    @sub_leader = (Role.includes(:user, :team).where(role: 2)).find { |item| item.user.id == params[:id].to_i }
+    @sub_leader_id = @sub_leader.user_id
+    @sub_leader_name = @sub_leader.user.name
+    @sub_leader_email = @sub_leader.user.email
+    @sub_leader_team = @sub_leader.team.name 
   end
 end
