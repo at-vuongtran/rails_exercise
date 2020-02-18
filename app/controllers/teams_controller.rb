b2a568f2a8d5f8d94a8a6ebc3e9ee638ed17e0ed
at-vuongtran <vuong.tran@asiantech.vn>
2020-02-18 10:29:05 +0700

    team show members and hightlight leader, subleader

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,5 +3,11 @@ class TeamsController < ApplicationController
     @teams = Team.all
   end
   
-  def show; end
+  def show
+    @team = Team.find(params[:id])
+    role_lead = RolesTable.find_by(name: "Leader").id
+    role_sub_lead = RolesTable.find_by(name: "Sub-Leader").id
+    @leader = UsersTeam.find_by(team_id: @team.id, role: role_lead)
+    @sub_leader = UsersTeam.find_by(team_id: @team.id, role: role_sub_lead)
+  end
 end
